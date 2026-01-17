e825056425f990bafcd382cd6b3a2c32d2b62a9d
Frankllin $c0tt <dellabrentraolane@gmail.com>
2026-01-17 19:58:06 +0200

    Refactor email sending function with SMTP settings

--- utils/email.js
+++ utils/email.js
@@ -2,10 +2,15 @@ import nodemailer from 'nodemailer';
 
 async function sendEmail(title="Password reset",email,username,message, resetMsg="") {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: 'smtp.gmail.com', 
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
     }
   });
 
@@ -18,9 +23,14 @@ async function sendEmail(title="Password reset",email,username,message, resetMsg
            <p>${resetMsg}</p>
            <p>If you did not request this, please ignore this email.</p>`
   };
-
+  try {
   await transporter.sendMail(mailOptions);
   console.log(`Password reset email sent to ${email}, reset: ${resetMsg}`);
+  return true;
+  } catch(err) {
+    console.error("Email Sending Errror: ", err);
+    throw new error("Email Service Timeout or Auth failure");
+  }
 }
 
 export default sendEmail;
