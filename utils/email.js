e17310418c4d9eed9c6a24d39884568f64c152b0
fRanklinscoT <dellabrentraolane@gmail.com>
2026-01-20 18:48:06 +0200

    Prod: added resend as it doesnt use SMTP

--- utils/email.js
+++ utils/email.js
@@ -1,32 +1,24 @@
-import nodemailer from 'nodemailer';
+import { Resend } from "resend";
 
-async function sendEmail(title="Password reset",email,username,message, resetMsg="") {
-  const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
+async function sendEmail(
+  title = "Password reset",
+  email,
+  username,
+  message,
+  resetMsg = ""
+) {
+  await resend.emails.send({
+    from: "MunicipalHub <onboarding@resend.dev>",
     to: email,
     subject: title,
-    html: `<p>Hi ${username},</p>
-           <p>${message}</p>
-           <p>${resetMsg}</p>
-           <p>If you did not request this, please ignore this email.</p>`
-  };
-  await new Promise((resolve, reject) => {
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error('Error sending email:', error);
-        reject(error);
-      } else {
-        resolve(info);
-      }
-    });
+    html: `
+      <p>Hi ${username},</p>
+      <p>${message}</p>
+      <p>${resetMsg}</p>
+      <p>If you did not request this, please ignore this email.</p>
+    `,
   });
 }
 
